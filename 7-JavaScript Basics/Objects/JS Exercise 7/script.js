91a2325fa53af21c21bfff3099323782eeca84a0
Jovan Nel <neljovan88@gmail.com>
2022-07-03 09:16:06 +0100

    JS Exercise 7 - Objects

---
+++ 7-JavaScript Basics/Objects/JS Exercise 7/script.js
@@ -0,0 +1,32 @@
+// Create an object that has properties "username" and "password". Fill those values in with strings.
+var userInfo = {
+	username: "root",
+	password: "password",
+};
+// Correct
+
+// Create an array which contains the object you have made above and name the array "database".
+var database = [
+	{
+		username: "root",
+		password: "password",
+	},
+];
+// Correct
+
+// Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "blah",
+		timeline: "boring",
+	},
+	{
+		username: "plah",
+		timeline: "depressing",
+	},
+	{
+		username: "klah",
+		timeline: "sad",
+	},
+];
+// Correct
