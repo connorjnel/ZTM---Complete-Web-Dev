9fe9ab7a1124739f382c10019b3748fdace45e88
Jovan Nel <neljovan88@gmail.com>
2022-07-04 08:54:28 +0100

    Uncommenting the loops

--- 7-JavaScript Basics/Loops/script.js
+++ 7-JavaScript Basics/Loops/script.js
@@ -1,50 +1,50 @@
 var todos = ["clean room", "exercise", "study js", "eat healthy"];
 
 // for - Add string to end of array item
-// for (var i = 0; i < todos.length; i++) {
-// 	todos[i] = todos[i] + "!";
-// }
+for (var i = 0; i < todos.length; i++) {
+	todos[i] = todos[i] + "!";
+}
 
 // for - Removes items from end of array, only runs until length equals i meaning two items left
-// for (var i = 0; i < todos.length; i++) {
-// 	todos.pop();
-// }
+for (var i = 0; i < todos.length; i++) {
+	// todos.pop();
+}
 
 // for -  Removes all items in array one by one, saved array length is var before running pop so length is not changing
-// var todosLength = todos.length;
-// for (var i = 0; i < todosLength; i++) {
-// 	todos.pop();
-// }
+var todosLength = todos.length;
+for (var i = 0; i < todosLength; i++) {
+	// todos.pop();
+}
 
 // while - Executes while true, overwrites original var value
-// var counterOne = 10;
-// while (counterOne > 0) {
-// 	console.log(counterOne);
-// 	counterOne--;
-// }
+var counterOne = 10;
+while (counterOne > 0) {
+	console.log(counterOne);
+	counterOne--;
+}
 
 // do -  Will execute at least once as action executes before condition is checked
-// var counterTwo = 10;
-// do {
-// 	console.log(counterTwo);
-// 	counterTwo--;
-// } while (counterTwo > 0);
+var counterTwo = 10;
+do {
+	console.log(counterTwo);
+	counterTwo--;
+} while (counterTwo > 0);
 
 // forEach - Simplified for function
 
 //  Standard forEach
-// todos.forEach(function (todo, i) {
-// 	console.log(todo, i);
-// });
+todos.forEach(function (todo, i) {
+	console.log(todo, i);
+});
 
 // Function saved and run by forEach on array
-// function logTodos(todo, i) {
-// 	console.log(todo, i);
-// }
+function logTodos(todo, i) {
+	console.log(todo, i);
+}
 
-// todos.forEach(logTodos);
+todos.forEach(logTodos);
 
 // Arrow function used
-// todos.forEach((i) => {
-// 	console.log(i);
-// });
+todos.forEach((i) => {
+	console.log(i);
+});
