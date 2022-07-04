cb19676b000171dc674df4ecb03f51ec814ce5f8
Jovan Nel <neljovan88@gmail.com>
2022-07-04 09:25:52 +0100

    Adding simple authentication to fakebook

--- 7-JavaScript Basics/Project - Build Facebook/script.js
+++ 7-JavaScript Basics/Project - Build Facebook/script.js
@@ -3,6 +3,14 @@ var database = [
 		username: "connor",
 		password: "password",
 	},
+	{
+		username: "sally",
+		password: "123",
+	},
+	{
+		username: "ingrid",
+		password: "777",
+	},
 ];
 
 var newsFeed = [
@@ -19,11 +27,23 @@ var newsFeed = [
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
 
+function isUserValid(user, pass) {
+	for (var i = 0; i < database.length; i++) {
+		if (database[i].username === user && database[i].password === pass) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/* If isUserValid is true then show newsfeed, if false use alert. 
+However if is executing if isUserValid true, boolean matches with the for loop wihtout having to write out expression saying only if true? 
+Added condition for true and still works just fine, interesting */
 function signIn(user, pass) {
-	if (user === database[0].username && pass === database[0].password) {
+	if (isUserValid(user, pass) === true) {
 		console.log(newsFeed);
 	} else {
-		alert("Intruder alert, wrong credentials entered!");
+		alert("Wrong credentials entered");
 	}
 }
 
