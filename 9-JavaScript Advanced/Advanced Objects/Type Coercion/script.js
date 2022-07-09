46f76e8653ea73953916e0d339dd3f9d0a8d878d
Jovan Nel <neljovan88@gmail.com>
2022-07-09 16:37:28 +0100

    Type Coercion

---
+++ 9-JavaScript Advanced/Advanced Objects/Type Coercion/script.js
@@ -0,0 +1,9 @@
+if (1 === "1") {
+	console.log("triple ", true);
+}
+
+if (1 == "1") {
+	console.log("double", true);
+}
+
+// Takeaway here is simple, if you dont wanna pull your hair out, use triple ===, not ==
