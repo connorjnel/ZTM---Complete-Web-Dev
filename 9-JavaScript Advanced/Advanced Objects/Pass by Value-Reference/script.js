26a93b1ee611f8e4766d8332d7570914289da61f
Jovan Nel <neljovan88@gmail.com>
2022-07-09 16:27:42 +0100

    Pass by value-reference

--- 9-JavaScript Advanced/Advanced Objects/Pass by Value-Reference/script.js
+++ 9-JavaScript Advanced/Advanced Objects/Pass by Value-Reference/script.js
@@ -13,3 +13,19 @@ let obj1 = {
 };
 
 let obj2 = obj1;
+
+// When decalring one obj to another obj, they reference the same memory location ie. the same value, so changing one will change the other.
+
+// Arrays, they are also objects
+
+var c = [1, 2, 3, 4, 5];
+var d = c;
+d.push(321321);
+
+// Cloning a object wihtout passing by reference
+
+let obj = { a: "a", b: "b", c: "c" };
+let objClone = Object.assign({}, obj);
+objClone.a = "a2";
+
+let superClone = JSON.parse(JSON.stringify(obj));
