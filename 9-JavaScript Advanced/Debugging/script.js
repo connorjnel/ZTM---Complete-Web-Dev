4782ed82b204d5a02eb1b57fac1005de22b3402f
Jovan Nel <neljovan88@gmail.com>
2022-07-11 12:27:58 +0100

    Debug notes

--- 9-JavaScript Advanced/Debugging/script.js
+++ 9-JavaScript Advanced/Debugging/script.js
@@ -9,8 +9,9 @@
 // Read through it and try to understand it
 // change arguments so they make more sense
 // add console logs to check function operation and troubleshoot
-// debugger - chrome dev tools causes stop to operation and freeezes, opens up console and shows where it stopped, shows output before stop
+// debugger -  causes stop to operation and opens chrome dev tools and freeezes, opens up console and shows where it stopped, shows output before stop
 // chrome dev tools sources, can use step over to check the operations and see what happens exactly
+// vscode has similar feature with run and debug, works similar to chrome dev tools debugger
 
 // fixed function
 const flattened = [
