b75d2712b26c20b01cca0cb61bf04989777c616d
Jovan Nel <neljovan88@gmail.com>
2022-07-11 12:06:40 +0100

    ES2021

---
+++ 9-JavaScript Advanced/ES2021/script.js
@@ -0,0 +1,14 @@
+// replaceALL = replace all occurences of a string, can assign to new variable
+
+const str = "ztm is the best of the best";
+
+str.replaceAll("best", "worst");
+
+// returns ("ztm is the worst of the worst");
+
+// replace - same thing but only did the first instance.
+
+// also works on plain string thats not in var
+
+"haha you are dumb haha".replaceAll("haha", "lol");
+// returns ("lol you are dumb lol");
