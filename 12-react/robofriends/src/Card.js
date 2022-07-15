09cadb42e2b20063ace3b3de4a37b1eb4b298125
Jovan Nel <neljovan88@gmail.com>
2022-07-15 15:42:17 +0100

    create robofriends part 3

--- 12-react/robofriends/src/Card.js
+++ 12-react/robofriends/src/Card.js
@@ -1,18 +1,16 @@
 import React from "react"; // import react itself
 import "./Card.css"; // import app stylesheet
 
-class Card extends React.Component {
-	render() {
-		return (
-			<div className="bg-blue dib br3 pa3 ma2 grow bw2 shadow-5 tc">
-				<img src={`https://robohash.org/${this.props.id}?200x200`} alt="robots" />
-				<div>
-					<h2>{this.props.name}</h2>
-					<p>{this.props.email}</p>
-				</div>
+const Card = ({ name, email, id }) => {
+	return (
+		<div className="bg-blue dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+			<img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Card;
--- 12-react/robofriends/src/index.js
+++ 12-react/robofriends/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client"; // import react dom for web
 import "./index.css"; // import base css stylesheet
 import reportWebVitals from "./reportWebVitals"; // import webvitals
 import "tachyons"; // import css library - need to switch to tailwind
-import CardList from "./CardList"; // cleaned up the card display to pull from seperate app
-import { robots } from "./robots"; // still need this as im using it here
+import App from "./App"; // cleaned up the card display to pull from seperate app
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // element selector
 root.render(
 	<React.StrictMode>
-		<CardList robots={robots} />
+		<App />
 	</React.StrictMode>
 );
 
