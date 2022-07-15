475439fce35c7d703075eadde2f115c5ab846855
Jovan Nel <neljovan88@gmail.com>
2022-07-15 13:54:43 +0100

    create robofriends part 2

--- 12-react/robofriends/src/index.js
+++ 12-react/robofriends/src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom/client"; // import react dom for web
 import "./index.css"; // import base css stylesheet
 import reportWebVitals from "./reportWebVitals"; // import webvitals
 import "tachyons"; // import css library - need to switch to tailwind
-import Card from "./Card"; // import apps, only have 1 for now
-import { robots } from "./robots";
+import CardList from "./CardList"; // cleaned up the card display to pull from seperate app
+import { robots } from "./robots"; // still need this as im using it here
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // element selector
 root.render(
 	<React.StrictMode>
-		<div>
-			<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-			<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-			<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-		</div>
+		<CardList robots={robots} />
 	</React.StrictMode>
 );
 
