32aff4084b006410be24938261a642770fd1c456
Jovan Nel <neljovan88@gmail.com>
2022-07-15 10:50:15 +0100

    first react component

---
+++ 12-react/robofriends/src/Hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hello.css";
+
+class Hello extends React.Component {
+	render() {
+		return (
+			<div className="hello">
+				<h1>This is a test, do not adjust your set</h1>
+				<p>Funny if this works</p>
+				<p>Yay, it worked &#x1F61C; </p>
+			</div>
+		);
+	}
+}
+
+export default Hello;
