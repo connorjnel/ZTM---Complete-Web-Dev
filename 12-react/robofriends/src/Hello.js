053c19c82f9fc73fe57c963911caa218f9af68e2
Jovan Nel <neljovan88@gmail.com>
2022-07-15 12:09:22 +0100

    creating components and props

--- 12-react/robofriends/src/Hello.js
+++ 12-react/robofriends/src/Hello.js
@@ -4,10 +4,11 @@ import "./Hello.css";
 class Hello extends React.Component {
 	render() {
 		return (
-			<div className="hello">
+			<div className="hello tc">
 				<h1>This is a test, do not adjust your set</h1>
 				<p>Funny if this works</p>
 				<p>Yay, it worked &#x1F61C; </p>
+				<p>{this.props.greeting}</p>
 			</div>
 		);
 	}
--- 12-react/robofriends/src/index.js
+++ 12-react/robofriends/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Hello from "./Hello";
+import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Hello />
+		<Hello greeting={"Hello React Ninja"} />
 	</React.StrictMode>
 );
 
