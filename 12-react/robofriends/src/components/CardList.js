701d9551ca91a8d34c4b0f79fe161729962ad101
Jovan Nel <neljovan88@gmail.com>
2022-07-16 17:23:13 +0100

    error boundary

--- 12-react/robofriends/src/components/CardList.js
+++ 12-react/robofriends/src/components/CardList.js
@@ -3,6 +3,9 @@ import Card from "./Card"; // import apps, only have 1 for now
 
 // destructuring method, dont need to import robots
 const CardList = ({ robots }) => {
+	if (true) {
+		throw new Error("NOOOOOOO!");
+	}
 	const cardComponent = robots.map((user, i) => {
 		return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />;
 	});
