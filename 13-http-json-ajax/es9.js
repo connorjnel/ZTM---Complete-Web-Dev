6c89c12c6acc327cc9316c27c4e547f8ef8bc313
Jovan Nel <neljovan88@gmail.com>
2022-07-18 12:12:05 +0100

    es9 new features

--- 13-http-json-ajax/es9.js
+++ 13-http-json-ajax/es9.js
@@ -22,7 +22,6 @@ function sum(a, b, c, d, e) {
 //Async . Finally
 
 //Example
-newFunction();
 
 function newFunction() {
 	const urls = ["http://swapi.dev/api/people/1", "http://swapi.dev/api/people/2", "http://swapi.dev/api/people/3", "http://swapi.dev/api/people/4"];
@@ -40,3 +39,36 @@ function newFunction() {
 	}
 	() => console.log("extra");
 }
+
+newFunction();
+
+// for await of
+
+const urls2 = ["https://jsonplaceholder.typicode.com/users", "https://jsonplaceholder.typicode.com/posts", "https://jsonplaceholder.typicode.com/albums"];
+
+const getData = async function () {
+	try {
+		const [users, posts, albums] = await Promise.all(urls2.map((url) => fetch(url).then((resp) => resp.json())));
+		console.log("users", users);
+		console.log("Posts", posts);
+		console.log("Albums", albums);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// Loops through urls
+const loopThroughUrls = (url) => {
+	for (url of urls2) {
+		console.log(url);
+	}
+};
+
+// new way to do it with await
+
+const getData2 = async function () {
+	const arrayOfPromises = urls2.map((url = fetch(url)));
+	for await (let request of arrayOfPromises) {
+		const data = await request.json();
+	}
+};
