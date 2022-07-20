c199f3e65187c86596c513e55e0232ed560f55e9
Jovan Nel <neljovan88@gmail.com>
2022-07-20 14:32:34 +0100

    smartbrain session 8

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -148,7 +148,16 @@ class App extends Component {
 		const image = document.getElementById("inputimage");
 		const width = Number(image.width);
 		const height = Number(image.height);
-		console.log(width, height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height,
+		};
+	};
+
+	displayFaceBox = (box) => {
+		this.setState({ box: box });
 	};
 
 	onInputChange = (event) => {
@@ -159,7 +168,7 @@ class App extends Component {
 		this.setState({ imageUrl: this.state.input });
 		app.models
 			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-			.then((response) => this.calculateFaceLocation(response))
+			.then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
 			.catch((error) => console.log(error));
 	};
 
@@ -171,7 +180,7 @@ class App extends Component {
 				<Logo />
 				<Rank />
 				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-				<FaceRecognition imageUrl={this.state.imageUrl} />
+				<FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
 			</div>
 		);
 	}
