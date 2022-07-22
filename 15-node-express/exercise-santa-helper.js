be1433b5ef41309de7c1604461c6bb738db37057
Jovan Nel <neljovan88@gmail.com>
2022-07-22 18:07:21 +0100

    santa helper exercise

--- 15-node-express/exercise-santa-helper.js
+++ 15-node-express/exercise-santa-helper.js
@@ -1,9 +1,20 @@
 import fs from "fs";
 
+// My crappy solution
+
 fs.readFile("./santa-directions.txt", (err, data) => {
 	console.time("challenge");
+	const instructions = data.toString();
+	// console.log(instructions);
 	let floor = 0;
-	if (data) console.timeEnd("challenge");
+	let count = 0;
+	for (let i = 0; i < instructions.length; i++) {
+		if (instructions === "(") {
+			count = floor + 1;
+		} else if (instructions === "(") {
+			count = floor - 1;
+		}
+	}
+	console.log(count);
+	console.timeEnd("challenge");
 });
-
-console.log("Async", data.toString());
