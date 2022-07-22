72fe15849d1e450bacb2d39a5197b3b1545302ef
Jovan Nel <neljovan88@gmail.com>
2022-07-22 18:43:24 +0100

    santa helper finish

--- 15-node-express/exercise-santa-helper.js
+++ 15-node-express/exercise-santa-helper.js
@@ -2,19 +2,70 @@ import fs from "fs";
 
 // My crappy solution
 
-fs.readFile("./santa-directions.txt", (err, data) => {
-	console.time("challenge");
-	const instructions = data.toString();
-	// console.log(instructions);
-	let floor = 0;
-	let count = 0;
-	for (let i = 0; i < instructions.length; i++) {
-		if (instructions === "(") {
-			count = floor + 1;
-		} else if (instructions === "(") {
-			count = floor - 1;
-		}
-	}
-	console.log(count);
-	console.timeEnd("challenge");
-});
+// fs.readFile("./santa-directions.txt", (err, data) => {
+// 	console.time("challenge");
+// 	const instructions = data.toString();
+// 	const directionsArray = instructions.split("");
+// 	let floor = 0;
+// 	let count = 0;
+// 	for (let i = 0; i < instructions.length; i++) {
+// 		if (instructions === "(") {
+// 			count = floor + 1;
+// 		} else if (instructions === "(") {
+// 			count = floor - 1;
+// 		}
+// 	}
+// 	console.log(count);
+// 	console.timeEnd("challenge");
+// });
+
+// Proper solution
+
+// Question 1 - What floor does santa end up on
+// ( = Up one floor
+// ) = Down one floor
+
+function question1() {
+	fs.readFile("./santa-directions.txt", (err, data) => {
+		console.time("challenge");
+		const directions = data.toString();
+		// console.log(directions); // Printed instruction correctly
+		const directionsArray = directions.split("");
+		const answer = directionsArray.reduce((acc, currentValue) => {
+			if (currentValue === "(") {
+				return (acc += 1);
+			} else if (currentValue === ")") {
+				return (acc -= 1);
+			}
+		}, 0);
+		console.timeEnd("challenge");
+		console.log("floor:", answer);
+	});
+}
+
+// question1();
+
+// Question 2 - When does Santa first enter the basement
+
+function question2() {
+	fs.readFile("./santa-directions.txt", (err, data) => {
+		console.time("challenge");
+		const directions = data.toString();
+		const directionsArray = directions.split("");
+		let accumulator = 0;
+		let counter = 0;
+		const answer = directionsArray.some((currentItem) => {
+			if (currentItem === "(") {
+				accumulator += 1;
+			} else if (currentItem === ")") {
+				accumulator -= 1;
+			}
+			counter++;
+			return accumulator < 0;
+		});
+		console.timeEnd("challenge");
+		console.log("Basement entered at:", counter);
+	});
+}
+
+question2();
