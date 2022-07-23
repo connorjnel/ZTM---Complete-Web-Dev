6664eb7368879e97f3ab673b0784c1facdc268b0
Jovan Nel <neljovan88@gmail.com>
2022-07-23 20:08:07 +0100

    smartbrain api

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -26,11 +26,12 @@ const database = {
 	],
 };
 
+// Root
 app.get("/", (req, res) => {
-	res.send("This is working");
+	res.send(database.users);
 });
 
-//Signin
+// Signin
 app.post("/signin", (req, res) => {
 	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
 		res.json("Success");
@@ -39,6 +40,36 @@ app.post("/signin", (req, res) => {
 	}
 });
 
+// Register
+app.post("/register", (req, res) => {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: "125",
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date(),
+	});
+	res.json(database.users[database.users.length - 1]);
+});
+
+// Profile
+app.get("/profile/:id", (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach((user) => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	});
+	if (!found) {
+		res.json("No such user found");
+	}
+});
+
+// Listener
 app.listen(3000, () => {
 	console.log("App is running on port 3000");
 });
