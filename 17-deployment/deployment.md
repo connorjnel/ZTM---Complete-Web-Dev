3c44fe43f802c59295d2797b10b3cd7ad600c333
Jovan Nel <neljovan88@gmail.com>
2022-08-01 23:21:13 +0100

    Code review

---
+++ 17-deployment/deployment.md
@@ -0,0 +1,11 @@
+# Deployment
+
+Taking a project with Frontend, backend, database and putting it online where its accessible
+Allows code to always be accessible without having to have it running on local system
+Heroku a good platform, also AWS, docker
+
+## Code Review
+
+Code reviews are methodical assessments of code designed to identify bugs, increase code quality, and help developers learn the source code. Developing a strong code review process, or utilizing version control, sets a foundation for continuous improvement and prevents unstable code from shipping to customers.
+
+- Set a intitial state so that users dont see input from other users
--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -135,24 +135,26 @@ const particleEffect = {
 	retina_detect: true,
 };
 
+const initialState = {
+	input: "",
+	imageUrl: "",
+	box: {},
+	route: "signin",
+	isSignedIn: false,
+	user: {
+		id: "",
+		name: "",
+		email: "",
+		// password: "",
+		entries: 0,
+		joined: "",
+	},
+};
+
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			input: "",
-			imageUrl: "",
-			box: {},
-			route: "signin",
-			isSignedIn: false,
-			user: {
-				id: "",
-				name: "",
-				email: "",
-				// password: "",
-				entries: 0,
-				joined: "",
-			},
-		};
+		this.state = initialState;
 	}
 
 	loadUser = (data) => {
@@ -161,7 +163,6 @@ class App extends Component {
 				id: data.id,
 				name: data.name,
 				email: data.email,
-				// password: data.password,
 				entries: data.entries,
 				joined: data.joined,
 			},
@@ -205,7 +206,8 @@ class App extends Component {
 						.then((response) => response.json())
 						.then((count) => {
 							this.setState(Object.assign(this.state.user, { entries: count }));
-						});
+						})
+						.catch(console.log);
 				}
 				this.displayFaceBox(this.calculateFaceLocation(response));
 			})
@@ -214,7 +216,7 @@ class App extends Component {
 
 	onRouteChange = (route) => {
 		if (route === "signout") {
-			this.setState({ isSignedIn: false });
+			this.setState(initialState);
 		} else if (route === "home") {
 			this.setState({ isSignedIn: true });
 		}
