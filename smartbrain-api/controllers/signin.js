177c8763aa648285b44c55c3a31e80eff5a3d664
Jovan Nel <neljovan88@gmail.com>
2022-08-02 16:27:23 +0100

    code review, creating controllers

---
+++ smartbrain-api/controllers/signin.js
@@ -0,0 +1,25 @@
+const handleSignin = (req, res, db, bcrypt) => {
+	db.select("email", "hash")
+		.from("login")
+		.where("email", "=", req.body.email)
+		.then((data) => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db
+					.select("*")
+					.from("users")
+					.where("email", "=", req.body.email)
+					.then((user) => {
+						res.json(user[0]);
+					})
+					.catch((err) => res.status(400).json("Cannot find user"));
+			} else {
+				res.status(400).json("Wrong Credentials");
+			}
+		})
+		.catch((err) => res.status(400).json("Incorrect login info"));
+};
+
+module.exports = {
+	handleSignin: handleSignin,
+};
--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -3,6 +3,11 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
 const db = knex({
 	client: "pg",
 	connection: {
@@ -33,95 +38,25 @@ app.get("/", (req, res) => {
 
 // Signin
 app.post("/signin", (req, res) => {
-	db.select("email", "hash")
-		.from("login")
-		.where("email", "=", req.body.email)
-		.then((data) => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash); // true
-			if (isValid) {
-				return db
-					.select("*")
-					.from("users")
-					.where("email", "=", req.body.email)
-					.then((user) => {
-						res.json(user[0]);
-					})
-					.catch((err) => res.status(400).json("Cannot find user"));
-			} else {
-				res.status(400).json("Wrong Credentials");
-			}
-		})
-		.catch((err) => res.status(400).json("Incorrect login info"));
+	signin.handleSignin(req, res, db, bcrypt);
 });
 
 // Register
 app.post("/register", (req, res) => {
-	const { email, name, password } = req.body;
-	const hash = bcrypt.hashSync(password);
-	db.transaction((trx) => {
-		trx
-			.insert({
-				hash: hash,
-				email: email,
-			})
-			.into("login")
-			.returning("email")
-			.then((loginEmail) => {
-				return trx("users")
-					.returning("*")
-					.insert({
-						email: loginEmail[0].email,
-						name: name,
-						joined: new Date(),
-					})
-					.then((user) => {
-						res.json(user[0]);
-					})
-					.then(trx.commit)
-					.catch(trx.rollback);
-			})
-			.catch((err) => res.status(400).json("Unable to register"));
-	});
+	register.handleRegister(req, res, db, bcrypt);
 });
 
 // Profile
 app.get("/profile/:id", (req, res) => {
-	const { id } = req.params;
-	db.select("*")
-		.from("users")
-		.where({ id: id })
-		.then((user) => {
-			if (user.length > 0) {
-				res.json(user[0]);
-			} else {
-				res.status(400).json("User not found");
-			}
-		})
-		.catch((err) => res.status(400).json("Error getting user"));
+	profile.handleProfileGet(req, res, db);
 });
 
 // Image / Submission Count
 app.put("/image", (req, res) => {
-	const { id } = req.body;
-	db("users")
-		.where("id", "=", id)
-		.increment("entries", 1)
-		.returning("entries")
-		.then((entries) => {
-			res.json(entries[0].entries);
-		})
-		.catch((err) => res.status(400).json("Unable to get count"));
+	image.handleImage(req, res, db);
 });
 
 // Listener
 app.listen(3000, () => {
 	console.log("App is running on port 3000");
 });
-
-/* API Plan
-/ res --> this is working
-/signin --> POST success/fail (using POST for security instead of GET)
-/register --> POST user 
-/profile/:userId --> GET = user
-/image --> PUT (update user profile) user (submission count)
-*/
