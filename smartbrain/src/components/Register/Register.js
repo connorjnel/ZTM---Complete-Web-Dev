5e04894d0016f34a9c7336d8025778e685d14519
Jovan Nel <neljovan88@gmail.com>
2022-08-02 16:45:58 +0100

    style tweaks

--- smartbrain/src/components/Register/Register.js
+++ smartbrain/src/components/Register/Register.js
@@ -47,28 +47,28 @@ class Register extends React.Component {
 				<main className="pa4 black-80">
 					<div className="measure">
 						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-							<legend className="f1 fw6 ph0 mh0">Register</legend>
+							<legend className="f1 fw6 ph0 mh0 white">Register</legend>
 							<div className="mt3">
-								<label className="db fw6 lh-copy f6" htmlFor="name">
+								<label className="db fw6 lh-copy f6 white" htmlFor="name">
 									Name
 								</label>
 								<input onChange={this.onNameChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
 							</div>
 							<div className="mt3">
-								<label className="db fw6 lh-copy f6" htmlFor="email-address">
+								<label className="db fw6 lh-copy f6 white" htmlFor="email-address">
 									Email
 								</label>
 								<input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
 							</div>
 							<div className="mv3">
-								<label className="db fw6 lh-copy f6" htmlFor="password">
+								<label className="db fw6 lh-copy f6 white" htmlFor="password">
 									Password
 								</label>
 								<input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
 							</div>
 						</fieldset>
 						<div className="">
-							<input onClick={this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib pointer" type="submit" value="Register" />
+							<input onClick={this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib pointer" type="submit" value="Register" />
 						</div>
 					</div>
 				</main>
